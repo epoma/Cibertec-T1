c7a76d439fd1981cf5022b0ce138a8f6ab874e65
epoma <edgardpoma@gmail.com>
2025-11-28 14:18:25 -0800

    button updated

--- Cibertec T1/ViewController.swift
+++ Cibertec T1/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Declare components
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterBtn: UIButton!
+            
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            print("1️⃣ viewDidLoad - Vista cargada en memoria")
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            print("2️⃣ viewWillAppear - Antes de aparecer")
+        }
 
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            print("3️⃣ viewDidAppear - Ya visible")
+        }
 
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            print("4️⃣ viewWillDisappear - Antes de desaparecer")
+        }
+
+        override func viewDidDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+            print("5️⃣ viewDidDisappear - Ya desapareció")
+        }
+        
+        @IBAction func enterBtn(_ sender: Any) {
+            print("Boton Pressed")
+            let emailTextFieldVar: String = emailTextField.text ?? "nothing"
+            print("email: \(emailTextFieldVar)")
+
+        }
+        
+    
 }
 
+	
