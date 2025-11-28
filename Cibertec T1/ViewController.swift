873783689120e1839e4929639212513c430940b2
epoma <edgardpoma@gmail.com>
2025-11-28 09:36:02 -0800

    Initial Commit

---
+++ Cibertec T1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cibertec T1
+//
+//  Created by Manager on 11/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
